48a9083fb2f569dcd46f067d2520daa881e246e0
tom-sapletta-com <tom@sapletta.com>
2023-01-21 20:16:49 +0100

    update for deployment

--- example.sh
+++ example.sh
@@ -17,3 +17,38 @@ info: Please wait a few second and go to url:
 url: http://text-to-web.poloshell.com/a2xqa2xmIGpna2xqZGtsZ2pkZmdqIGRn
 question: Are you happy with the result? What should be changed?
 - reply by Text:
+
+
+
+
+
+#!/bin/usr/dialogmap
+# PoLoShell is text-to-software solution.
+# Choose type of software, you want build:
+# 0. help
+# 1. API service
+# 2. WEB page
+# 3. VUE component
+# After You selected, describe in a few sentence your expectations
+# It will by deployed on personalized url for You!
+q: Choose type of software, you want build?
+# reply by Number:
+a: 0
+
+? Choose type of software, you want build?
+# reply by Number:
+- 0
+# Choose type of software, you want build
+# 0. help
+# 1. API service
+# 2. WEB page
+# 3. VUE component
+Q: Choose type of software, you want build?
+A: reply by Number: 1
+# API service
+question: What should do the API, describe please?
+- reply by Text: kjh fdskjhj dhgsdf hgf jhd gghdf
+# Please wait a few second and go to url:
+url: http://text-to-api.poloshell.com/a2poIGZkc2tqaGogZGhnc2RmIGhnZiBqaGQgZ2doZG
+Yg
+question: Are you happy with the result? What should be changed?  - reply by Text:
