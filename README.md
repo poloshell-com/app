67d6209320b4897996083baca472879d2c12c084
tom-sapletta-com <tom@sapletta.com>
2023-01-21 20:31:33 +0100

    update for deployment

--- README.md
+++ README.md
@@ -2,8 +2,14 @@
 
 
 
-
+## Old version
 ![Poloshell3](poloshell3.png)
+
+## dialogmap
+![dialogmap](dialogmap.png)
+
+## grep
+![grep](grep.png)
 
 [GitHub Corners](https://tholman.com/github-corners/)
 
---
+++ dialogmap.sh
@@ -0,0 +1,22 @@
+#!/bin/usr/dialogmap
+# PoLoShell is text-to-software solution.
+# Choose type of software, you want build:
+# 0. help
+# 1. API service
+# 2. WEB page
+# 3. VUE component
+# After You selected, describe in a few sentence your expectations
+# It will by deployed on personalized url for You!
+
+Q: Choose type of software, you want build?
+# reply by Number
+A: 2
+# Web page
+Q: What should do the page, describe please?
+# reply by Text
+A: d adkl jdkjklsjlkjgfd ldfg fd
+# Please wait a few second and go to url:
+url: http://text-to-web.poloshell.com/ZCBhZGtsIGpka2prbHNqbGtqZ2ZkIGxkZmcgZmQ=
+Q: Are you happy with the result? What should be changed?
+# reply by Text
+A:
